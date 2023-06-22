d0c1acca4c70803e95a02acf06b6b322118763cb
Alberto Re <alberto.re@gmail.com>
2023-06-22 16:42:41 +0200

    Explicitly require nvim-web-devicons

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -44,6 +44,8 @@ return packer.startup(function(use)
 
 	use("lukas-reineke/indent-blankline.nvim")
 
+	use("nvim-tree/nvim-web-devicons")
+
 	use({
 		"nvim-lualine/lualine.nvim",
 		requires = { "nvim-tree/nvim-web-devicons", opt = true },
