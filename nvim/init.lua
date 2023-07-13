2a8e2d201ec64321fc6ddafde7c4624c5216c9f3
Alberto Re <alberto.re@gmail.com>
2023-07-13 11:22:46 +0200

    Minor change in telescope configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,11 +35,10 @@ lsp.setup()
 -- Telescope
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fc", builtin.commands, {})
 vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 vim.keymap.set("n", "<leader>gs", builtin.grep_string, {})
 vim.keymap.set("n", "<leader>gl", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 -- Diagnostics
 
