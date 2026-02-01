b959029314495afe45714037295344e9b17abd16
Alberto Re <alberto.re@gmail.com>
2026-02-01 08:58:05 +0100

    New Neovim configuration from scrath.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,103 +1,66 @@
---
--- Plugins configuration
---
+-- Requires version 0.12 or above.
 
-require("user.plugins")
+-- Get rid of annoying "Undefined global `vim`" warning due
+-- to lua_ls not recognizing where the variable comes from.
+---@diagnostic disable: undefined-global 
 
--- Theme
-
-vim.cmd("colorscheme rose-pine")
-
-require("ibl").setup()
-
--- Completion
-local cmp = require("cmp")
-
-cmp.setup({
-	mapping = cmp.mapping.preset.insert({
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-	}),
-  formatting = {
-    fields = {"abbr", "kind", "menu"},
-    format = require("lspkind").cmp_format({
-      mode = "symbol",
-      maxwidth = 50,
-      ellipsis_char = "...",
-    })
-  }
-})
-
--- Statusline
-
--- As stated in the lualine README to enable icons install a patched font
--- like this for example: https://github.com/ryanoasis/nerd-fonts
-require("lualine").setup()
-
---
--- Options
---
-
-vim.cmd([[set ff=unix fixeol]])
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
+-- Disable the use of a swapfile for the buffer.
+vim.opt.swapfile = false
+-- Number of columns per indentation.
+vim.opt.shiftwidth = 4
+-- Insert <Tab> as an appropriate number of spaces.
 vim.opt.expandtab = true
+-- Number of columns to use to display <Tab>.
+vim.opt.tabstop = 4
+-- Ignore case in search patterns.
+vim.opt.ignorecase = true
+-- Print the line number in front of current line, and the line number
+-- relative to the line with the cursor in front of each other line.
 vim.opt.number = true
 vim.opt.relativenumber = true
+-- Border style for floating windows.
+vim.opt.winborder = "rounded"
+-- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+-- Sync with system clipboard.
 vim.opt.clipboard = "unnamedplus"
 
---
--- Custom keyboard mappings
---
-
--- Map <leader> to space: easy to reach with both hands
-vim.g.mapleader = " "
-
--- Re-execute last executed command
-vim.keymap.set("n", "<leader>r", ":!!<cr>", {})
-
--- Quickly move between next/previous open buffer
-vim.keymap.set("n", "<leader>n", ":bnext<cr>", {})
-vim.keymap.set("n", "<leader>b", ":bprev<cr>", {})
-
--- Reformat currently open buffer if LSP supports it
-vim.keymap.set("n", "<leader><space>", function()
-  vim.lsp.buf.format { async = true }
-end, {})
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, {})
-
--- Searching
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>gl", builtin.live_grep, {})
-
-require("telescope").setup({
-    pickers = {
-        find_files = {
-            layout_config = {
-                prompt_position = "top",
-                width = 0.95,
-                height = 0.95,
-            },
-            sorting_strategy = "ascending",
-        },
-        live_grep = {
-            layout_config = {
-                prompt_position = "top",
-                width = 0.95,
-                height = 0.95,
-            },
-            sorting_strategy = "ascending",
-        },
-
-    },
+-- Declare plugins to install.
+vim.pack.add({
+    { src = "https://github.com/numToStr/Comment.nvim" },
+    { src = "https://github.com/nvim-mini/mini.pick" },
+    { src = "https://github.com/rose-pine/neovim" },
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
 })
 
--- Diagnostics
-vim.diagnostic.config({ virtual_text = false })
-vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {})
-vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {})
+-- Enable installed plugins.
+require("Comment").setup()
+require("mini.pick").setup()
+require("rose-pine").setup()
+
+-- Enable LSP for languages we care about.
+-- Run ':checkhealth vim.lsp' for diagnostics.
+-- LSP implementations must be installed manually.
+vim.lsp.enable("ruff")
+vim.lsp.enable("lua_ls")
+
+-- Display LSP messages inline.
+vim.diagnostic.config({ virtual_text = true })
+
+-- Set color theme.
+vim.cmd("colorscheme rose-pine")
+
+-- Map <leader> to spacebar.
+vim.g.mapleader = " "
+
+-- Leader mappings.
+-- Mappings for 'mini.pick'.
+vim.keymap.set("n", "<Leader>ff", "<Cmd>Pick files<CR>", { desc = "Search files" })
+vim.keymap.set("n", "<Leader>fb", "<Cmd>Pick buffers<CR>", { desc = "Search buffers" })
+vim.keymap.set("n", "<Leader>gf", "<Cmd>Pick grep_live<CR>", { desc = "Grep files" })
+vim.keymap.set("n", "<Leader>gc", "<Cmd>Pick grep pattern='<cword>'<CR>", { desc = "Grep current word" })
+-- Mappings for Language Server Protocol.
+vim.keymap.set("n", "<Leader>lf", vim.lsp.buf.format, {})
+-- Quality of life mappings.
+vim.keymap.set("n", "<Leader>w", "<CMD>w<CR>", { desc = "Save current file" })
+vim.keymap.set("n", "<Leader>q", "<CMD>q<CR>", { desc = "Quit current file" })
